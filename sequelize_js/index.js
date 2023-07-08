48dd22c1b4121aad3d48083b64a8a4f38a9af0d6
stevehotcodes <stevehotcodes@gmail.com>
2023-07-08 16:35:02 +0300

    added models and constraints to decodeUsers and questions models

--- sequelize_js/index.js
+++ sequelize_js/index.js
@@ -1,4 +1,5 @@
 const {Sequelize}=require('sequelize');
+const { IsEmail, AutoIncrement } = require('sequelize-typescript');
 
 const sequelize =new Sequelize('TheJituDB','sa','1234',{
     host:'localhost',                                            //create the sequelize instance
@@ -16,13 +17,13 @@ catch(error){                                                         //connect 
    console.error('unable to connect to the database', error);
 }
 
-finally{
-    setTimeout(()=>{
-        sequelize.close();
-        console.log("connection close")
-    },10000)
+// finally{
+//     setTimeout(()=>{
+//         sequelize.close();
+//         console.log("connection close")
+//     },10000)
     
-}
+// }
 
 }
 connectDatabase()
@@ -30,4 +31,144 @@ connectDatabase()
 // sequelize.close()
 
 
-// 
+//create the model
+const decodeUsers= sequelize.define(
+    "decodeUsers",
+    {   
+        id:
+        {
+            type:Sequelize.INTEGER,
+            unique:true,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        firstName:{
+            type:Sequelize.STRING,
+            unique:false,
+            allowNull:false
+        },  
+        lastName:{
+            type:Sequelize.STRING,
+            unique:false,
+            allowNull:false
+        },
+          
+        email:{
+            type:Sequelize.STRING,
+            unique:false,
+            allowNull:false,
+           validate:{
+                isEmail:true
+           }
+        },
+        userName:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            unique:true
+        },
+
+        github:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            unique:false
+        },
+        password:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            validate:{
+                notEmpty:true,
+                isAlphanumeric:true,
+                len:[1,30]
+            }
+        },
+      
+        dateJoined:{
+            type:Sequelize.DATE,
+            defaultValue:Sequelize.NOW // or default date option two default "defaultValue:()=> new Date()"
+        }
+    }
+
+    
+)
+//questions Model
+const questions =sequelize.define(
+    'questions',
+    {
+        id:{
+                type:Sequelize.UUID,
+                allowNull:false,
+                unique:true,
+                primaryKey:true
+
+            },
+        questionTitle:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            validate:{
+                notEmpty:true,
+                len:[3,50]
+               
+            }
+
+        },
+        questionDescription:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            valiidate:{
+                notEmpty:false
+            }
+        },
+        questionTag:{
+            type:Sequelize.STRING,
+            allowNull:false,
+            validate:
+            {
+                notEmpty:true
+            }
+        }
+    }
+    
+    
+    
+    )
+
+//synchronize with the database
+sequelize.sync()
+    .then(()=>{
+        console.log('database and tables created')
+    })
+    .catch((error)=>{
+        console.error('error in creating database', error)
+    })
+
+
+//hook for aftercreate user
+decodeUsers.afterCreate(async (decodeUsers,options)=>{
+    console.log("New User has been created:");
+    console.log(decodeUsers.userName);
+    console.log(decodeUsers.email)
+})
+
+
+//insert data
+
+async function createUser(){
+    try {
+        const user =await decodeUsers.create({
+            firstName:"Vladimir",
+            lastName:"Zelensky",
+            userName:"Vladimir",
+            email:"vladimirzelenesky@gmail.com",
+            github:"vladzelenesky",
+            password:"Password123"
+        
+        })
+        console.log(`user ${user.userName} has been created successfully`)
+        
+    } catch (error) {
+        console.log("error in creating user", error)
+        
+    }
+}
+createUser()
+  
